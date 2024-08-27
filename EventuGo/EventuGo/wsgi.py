e0c107ad5ff7c88ad49d2a1a953b7ef77a70907b
Daniel Reis <129684215+Danielpyreis@users.noreply.github.com>
2024-08-27 18:49:54 +0000

    Arrumando configs

--- EventuGo/EventuGo/wsgi.py
+++ EventuGo/EventuGo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EventoGo project.
+WSGI config for EventuGo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventoGo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventuGo.settings')
 
 application = get_wsgi_application()
