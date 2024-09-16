97546133b82ce3af694715f3d3c1b1cd714b8f14
icarompo <icaromesquitaponce@outlook.com>
2024-09-16 09:18:29 -0300

    feat: adiciona register dos models no django admin

--- EventuGo/credits/admin.py
+++ EventuGo/credits/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from credits.models import HistoricoTransacao, Saldo, Transacao
 
 # Register your models here.
+admin.site.register(Saldo)
+admin.site.register(Transacao)
+admin.site.register(HistoricoTransacao)
--- EventuGo/evento/admin.py
+++ EventuGo/evento/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from evento.models import Evento
 # Register your models here.
+
+admin.site.register(Evento)
