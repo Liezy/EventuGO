3bcecf762abc85bd90569c8b225afaca5419762c
Danielpyreis <danielreis04@live.com>
2024-10-05 15:16:06 -0300

    up

--- backend/EventuGo/settings.py
+++ backend/EventuGo/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'EventuGo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,11 +118,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+#LOGIN_REDIRECT_URL = 'home'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Diretório global de arquivos estáticos
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, CustomLoginView
 
 router = DefaultRouter()
 router.register(r'usuarios', UserViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+     path('login/', CustomLoginView.as_view(), name='login'),
 ]
