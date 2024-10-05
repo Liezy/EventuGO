b742fea8b6ad1a199f7dd8675515e73a006bd047
Liezy <alencareliezer20@gmail.com>
2024-10-05 19:59:34 -0300

    fix: alterando endpoint de login

--- backend/EventuGo/urls.py
+++ backend/EventuGo/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('event/', include('event.urls')),
     path('users/', include('users.urls')),
     path('company/', include('company.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
