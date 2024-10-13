6b4881e7eec7b4981c1a5997c7cac3896054c691
icarompo <icaromesquitaponce@outlook.com>
2024-10-13 18:19:41 -0300

    fix: arquivo de dependencias e rota padrao para web admin

--- backend/EventuGo/urls.py
+++ backend/EventuGo/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import MyTokenObtainPairView
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,6 @@ urlpatterns = [
     path('company/', include('company.urls')),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/', include('authentication.urls')),  # Inclui o app de autenticação
+    path('', lambda request: redirect('home')), 
 ]
