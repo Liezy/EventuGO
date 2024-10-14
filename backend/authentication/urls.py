7cc2d905d561d13090def2c88551ac7f7d411060
reis.sales <reis.sales@mail.uft.edu.br>
2024-10-14 10:55:42 -0300

    up/Dasboard_Eventos

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import login_view, register_user, HomeView, logout_view
+from .views import login_view, register_user, HomeView, logout_view, EventView, EventEditView, EventCreateView
 
 urlpatterns = [
     path('login/', login_view, name="login"),        
     path('register/', register_user, name="register"), 
     path('logout/', logout_view, name="logout"),
-    path('home/', HomeView.as_view(), name="home") 
+    path('home/', HomeView.as_view(), name="home"), 
+    path('eventos/', EventView.as_view(), name="eventos"),
+    path('eventEdit/<int:pk>/', EventEditView.as_view(), name="eventEdit"),
+    path('eventCreate/', EventCreateView.as_view(), name="eventCreate")
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,13 +1,14 @@
 from typing import Any
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, SignUpForm, EventForm
 from users.models import CustomUser
 from event.models import Transaction, Event
 from company.models import Company
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView, CreateView
 from django.db import IntegrityError
+from django.urls import reverse_lazy
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home/index.html'  # Template que será renderizado
@@ -21,6 +22,44 @@ class HomeView(LoginRequiredMixin, TemplateView):
         context['total_usuarios'] = CustomUser.objects.count()
         context['total_empresas'] = Company.objects.count()
         context['transacoes'] = Transaction.objects.all() 
+        return context
+    
+    
+    
+class EventView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/event.html'
+    
+    def get_context_data(self, **kwargs):
+        context =super().get_context_data(**kwargs)
+        
+        context['events'] = Event.objects.all()
+        return context
+class EventEditView(LoginRequiredMixin, UpdateView):
+    model = Event
+    template_name = 'home/event_edit.html'
+    fields = ['name', 'description', 'type', 'start_date', 'end_date']
+    success_url = reverse_lazy('eventos')  # Redireciona para a lista de eventos
+
+    def get(self, request, *args, **kwargs):
+        if 'pk' not in kwargs:
+            return redirect('eventos')  
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"Editar Evento: {self.object.name}"
+      
+        context['form'] = self.get_form()
+        return context
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    form_class = EventForm
+    template_name = 'home/event_create.html'
+    success_url = reverse_lazy('eventos')  
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Criar Novo Evento"
         return context
 def login_view(request):
     form = LoginForm(request.POST or None)
