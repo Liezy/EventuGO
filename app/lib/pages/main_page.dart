d7e096fc17cf714cd9e72e9b885e68f6704affba
Ícaro Mesquita Ponce <130798813+icarompo@users.noreply.github.com>
2024-10-14 14:13:20 -0300

    Update main_page.dart

--- app/lib/pages/main_page.dart
+++ app/lib/pages/main_page.dart
@@ -1,12 +1,15 @@
+import 'package:app/src/pages/events/listagem_eventos.dart';
+import 'package:app/src/pages/profile/perfil_usuario.dart';
 import 'package:flutter/material.dart';
-import 'package:app/pages/home_page.dart';
-import 'package:app/pages/auth/user_register_page.dart';
-import 'package:app/pages/manage_events/manage_events.dart';
-import 'package:app/pages/creditos_page.dart';
-import 'package:app/pages/transacao_qr_page.dart'; // Importa o gerador de qr code
-import 'package:app/pages/consulta_saldo_historico_page.dart';
-import 'package:app/pages/qr_code_scan.dart'; // Importa o scanner
-import 'package:app/pages/auth/login_page.dart';
+import 'package:app/src/pages/home/home_page.dart';
+import 'package:app/src/pages/auth/sign_up.dart';
+import 'package:app/src/pages/rotas_wip/manage_events/manage_events.dart';
+import 'package:app/src/pages/rotas_wip/creditos_page.dart';
+import 'package:app/src/pages/events/transacao_qr_page.dart'; // Importa o gerador de qr code
+import 'package:app/src/pages/consulta_saldo_historico_page.dart';
+import 'package:app/src/pages/qr_code_scan.dart'; // Importa o scanner
+import 'package:app/src/pages/auth/sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -24,6 +27,17 @@ class _MainPageState extends State<MainPage> {
     _pageController.jumpToPage(index);
   }
 
+  Future<void> _logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('user_uid'); // Remove o UUID salvo
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+          builder: (context) =>
+              SignInPage()), // Redireciona para a página de login
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,56 +49,62 @@ class _MainPageState extends State<MainPage> {
           });
         },
         children: [
-          LoginPage(),
-          HomePage(), // Exibe a HomePage
-          CadastroUsuarioPage(), // Tela de Cadastro de Usuário
-          ManageEventsPage(), // Tela de Cadastro de Evento
-          CreditosPage(), // Tela de Créditos
-          TransacaoQrPage(), // Nova Tela de Transação com QR Code
-          ConsultaSaldoHistoricoPage(), // Tela de consulta de saldo e histórico
-          QrCodeScanPage(), // Nova Tela de Escaneamento de QR Code
+          HomePage(),
+          UserEventsPage(),
+          UserProfilePage(),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Login',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Usuário',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event),
-            label: 'Gerenciar Eventos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_balance_wallet),
-            label: 'Créditos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code),
-            label: 'QR Code', // Novo item no menu para QR Code
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'Consulta', // Novo item para a tela de consulta
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera), // Ícone para escanear QR Code
-            label: 'Escanear', // Novo item no menu
-          ),
-        ],
+      bottomNavigationBar: Container(
+        height: 70, // Aumenta a altura do BottomNavigationBar
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white, // Cor de fundo do BottomNavigationBar
+          currentIndex: _currentIndex,
+          onTap: _onTabTapped,
+          selectedItemColor: Colors.purple[700],
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.event),
+              label: 'Eventos',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Perfil',
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: Container(
+        width: 100, // Define a largura do botão flutuante
+        height: 56, // Define a altura do botão flutuante
+        child: FloatingActionButton(
+          onPressed: _logout,
+          backgroundColor: Colors.red, // Cor de fundo do botão flutuante
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero, // Remove os cantos arredondados
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.exit_to_app,
+                  color: Colors.white), // Ícone de sair em branco
+              SizedBox(width: 5), // Espaçamento entre o ícone e o texto
+              Text(
+                'Sair',
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Texto 'Sair' em branco
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.endTop, // Localização do botão flutuante
     );
   }
 }
