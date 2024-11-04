97d838a388373d05a53de7380d64283c09c7adac
icarompo <icaromesquitaponce@outlook.com>
2024-11-04 14:03:16 -0300

    fixing local bugs

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu App',
+      title: 'EventuGo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
